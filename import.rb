93cab85c91a93b9ef77561f26573728989086320
Rick Conrad <richardjconrad@gmail.com>
2017-10-24 11:29:59 +0100

    Add children to people table and children table. Numerous other improvements

--- import.rb
+++ import.rb
@@ -1,20 +1,45 @@
 require 'pg'
+require 'date'
+
+# FILE IO ======================================================
 
 def read_file(filename)
+  filename = "csv/#{filename}"
   File.open(filename, 'r') do |csvfile|
-    headers = csvfile.gets.split('|')
+    headers = csvfile.gets.chomp.split('|')
     
+    linenumber = 1
     while line = csvfile.gets
+      line.chomp!
       params = {}
       fields = line.split('|')
       fields.each_with_index do |field, i|
         params[headers[i]] = field
       end
+      params['linenumber'] = linenumber
+      linenumber += 1
 
+      puts "PARAMS FROM FILE: #{params}"
       yield(params)
 
     end
   end
+end
+
+# PARSE Helpers =================================================
+
+def dequote(s)
+  return if s.nil?
+  s = s.chomp.chomp('"')
+  s.slice!(0) if s[0] == '"'
+  s
+end
+
+def invalid_date?(y, m, d)
+  return true unless (1900 .. Date.today.year) === y.to_i 
+  return true unless (1 .. 12) === m.to_i
+  return true unless (1 .. 31) === d.to_i  # yeah, it's not 100% reliable
+  false
 end
 
 def extract_date(s, format)
@@ -22,13 +47,10 @@ def extract_date(s, format)
   y = s[format.index('yyyy'), 4]
   m = s[format.index('mm'), 2]
   d = s[format.index('dd'), 2]
-  return "#{y}-#{m}-#{d}"
-end
-
-def query(conn, sql, params)
-  puts sql
-  puts params
-  conn.exec_params(sql, params)
+  if (1 .. 200) === y.to_i
+    y = 1900 + y.to_i
+  end
+  invalid_date?(y, m, d) ? nil : "#{y}-#{m}-#{d}"
 end
 
 def timestamps
@@ -36,30 +58,83 @@ def timestamps
 end
 
 def prepare_params(params)
-  params.transform_values!{ |v| v.gsub("'", "\\'") if v.respond_to? :gsub }
-  params.merge timestamps
+  params.transform_values! do |v| 
+    v.respond_to?(:gsub) ? v.gsub("'", "\\'") : v  
+  end
+  params.merge! timestamps
+  puts "PARAMS: #{params}"
+  params
+end
+
+# DB Helpers =========================================================
+
+def query(conn, sql, params=nil)
+  puts "#{sql} [#{params}]"
+  params.nil? ? 
+          conn.exec(sql) :
+          conn.exec_params(sql, params)
+end
+
+def find(conn, table, id)
+  result = query conn, "SELECT * FROM #{table} WHERE id=#{id};"
+  (result.ntuples > 0) ? result[0] : nil
+end
+
+def find_last(conn, table)
+  result = query conn, "SELECT * FROM #{table} ORDER BY id DESC LIMIT 1;"
+  result[0]
+end
+
+def exists?(conn, table, id)
+  result = query conn, "SELECT id FROM #{table} WHERE id=#{id};"
+  return result.ntuples > 0
+end
+
+def update_sequence(conn, table)
+  sequence_res = conn.exec("SELECT pg_get_serial_sequence('#{table}', 'id');")
+  sequence = sequence_res[0]['pg_get_serial_sequence']
+  seq_val = conn.exec("SELECT last_value FROM #{sequence};")[0]['last_value']
+  biggest_id = conn.exec("SELECT MAX(id) FROM #{table};")[0]['max']
+  
+  unless seq_val > biggest_id
+    restart = biggest_id.to_i + 1
+    conn.exec("ALTER SEQUENCE #{sequence} RESTART WITH #{restart};")
+  end
+end
+
+def insert(conn, table, params)
+  params = prepare_params params
+  fields = params.keys
+  sql = "INSERT INTO #{table} ("
+  sql += fields.join(', ')
+  sql += ") VALUES("
+  sql += fields.collect{ |field| "'#{params[field]}'"}.join(', ')
+  sql += ");"
+  puts query(conn, sql).cmd_status
+  update_sequence(conn, table) if params[:id]
+end
+
+def update(conn, table, params)
+  params = prepare_params params
+  fields = params.keys
+  sql = "UPDATE #{table} SET "
+  sql += fields.collect{ |field| "#{field} = '#{params[field]}'"}.join(', ')
+  sql += " WHERE id=#{params[:id]};"
+  puts query(conn, sql).cmd_status
 end
 
 def insert_or_update(conn, table, params)
-  params = prepare_params params
   sql = "SELECT id FROM #{table} WHERE id=$1;"
   existing = query(conn, sql, [params[:id]])
 
-  fields = params.keys
-  if existing.ntuples > 0
-    sql = "UPDATE #{table} SET "
-    sql += fields.collect{ |field| "#{field} = '#{params[field]}'"}.join(', ')
-    sql += " WHERE id=#{params[:id]};"
+  if exists?(conn, table, params[:id])
+    update conn, table, params
   else
-    sql = "INSERT INTO #{table} ("
-    sql += fields.join(', ')
-    sql += ") VALUES("
-    sql += fields.collect{ |field| "'#{params[field]}'"}.join(', ')
-    sql += ");"
+    insert conn, table, params
   end
-  puts sql
-  conn.exec sql
 end
+
+# Employee Import ====================================================
 
 def person_names(params)
   name = (params['Name']=='') ? params['ShortName'] : params['Name']
@@ -74,7 +149,7 @@ def person_names(params)
   return first, last
 end
 
-def add_person(conn, params)
+def add_employee_person(conn, params)
   first, last = person_names params
   gender = (params['Gender']=='F') ? '1' : '0'
   birth = extract_date params['BirthDate'], 'dd/mm/yyyy'
@@ -83,7 +158,60 @@ def add_person(conn, params)
   insert_or_update conn, 'people', save_params
 end
 
+# Child Import ====================================================
+
+def child_names(name)
+  split = name.index(' ')
+  if split.nil?
+    first = name
+    last = ''
+  else
+    last = name[0, split]
+    first = name[split+1 .. -1].lstrip
+  end
+  return first, last
+end
+
+def insert_child(conn, person_params, child_params)
+  conn.transaction do |conn|
+    insert conn, 'people', person_params
+    person = find_last conn, 'people'
+    child_params[:person_id] = person['id']
+    insert conn, 'children', child_params
+  end
+end
+
+def update_child(conn, child, person_params, child_params)
+  update conn, 'children', child_params
+  person_params[:id] = child['person_id']
+  update conn, 'people', person_params
+end
+
+def add_child(conn, params)
+  # Don't add children if the parent doesn't exist
+  return unless exists? conn, 'people', params['EmployeeId']
+
+  first, last = child_names params['Name']
+  birth = extract_date params['BirthDate'], 'dd/mm/yyyy'
+  person_params = {first_name: first, 
+                   last_name: last,}
+  person_params[:birth_date] = birth unless birth.nil?                   
+  child_params = {id: params['linenumber'], 
+                  parent_id: params['EmployeeId'], 
+                  is_student: params['Student']}
+  puts "CHILD PARAMS: #{child_params}"
+
+  child = find conn, 'children', child_params[:id]
+  if child.nil?
+    insert_child conn, person_params, child_params
+  else
+    update_child conn, child, person_params, child_params
+  end
+end
+
+
 # ============ START HERE ===================================
+# ===========================================================
 
 conn = PG.connect(dbname: 'cmbpayroll_dev',
                   host: 'localhost',
@@ -91,49 +219,11 @@ conn = PG.connect(dbname: 'cmbpayroll_dev',
                   password: 'cmbpayroll')
 
 read_file('employees.csv') do |params|
-  add_person conn, params
+  add_employee_person conn, params
+  puts ''
 end
 
-
-# ========== CSVImporter for model ==========================
-
-class CSVImporter
-  
-    def self.import(filename, model, import_fields={})
-      failed_records = []
-      File.open(filename, 'r') do |csvfile|
-        headers = csvfile.gets.split('|')
-        headers.each_index { |i| headers[i] = dequote(headers[i]) }
-  
-        while line = csvfile.gets
-          fields = {}
-          farray = line.split('|')
-          headers.each_index { |i| fields[headers[i]] = dequote(farray[i]) }
-  
-          params = {}
-          import_fields.each_pair{ |model_sym, old_name| params[model_sym] = fields[old_name] }
-          
-          import_record = model.find_by(id: params[:id])
-          import_record ||= model.new
-  
-          params = yield(params) if block_given?
-  
-          begin
-              import_record.update params
-          rescue => error
-              failed_records << "Failed to import:\n  #{line}\n  Because #{error.message}\n"
-          end
-        end
-      end
-      failed_records.each{|line| p line}
-    end
-  
-    def self.dequote(s)
-      return if s.nil?
-      s = s.chomp.chomp('"')
-      s.slice!(0) if s[0] == '"'
-      s
-    end
-  
-
-  end
+read_file('children.csv') do |params|
+  add_child conn, params
+  puts ''
+end
